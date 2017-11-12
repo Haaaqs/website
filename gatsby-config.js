31ee8b0786740010dbf1ecc06ce1099c50545b02
aouerfelli <a.ouerf@gmail.com>
2017-11-12 11:49:46 -0500

    Set up website config and dev tools

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,37 @@
+const config = require('./src/data/config');
+
 module.exports = {
+  // Must not have trailing slash
+  pathPrefix: config.pathPrefix.replace(/\/+$/, ''),
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: config.metadata.title,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // TODO: Set Google Analytics tracking ID (in config)
+        trackingId: config.googleAnalyticsId,
+        anonymize: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.metadata.title,
+        short_name: config.metadata.label,
+        description: config.metadata.desc,
+        start_url: config.pathPrefix,
+        display: 'minimal-ui',
+        // TODO: Change theme colors (in config)
+        theme_color: config.colors.theme,
+        background_color: config.colors.background,
+        // TODO: Add favicons
+        icons: [],
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+};
