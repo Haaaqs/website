b50cfd4a9161af5125675a9fb89a6d6599498ad2
Affan Haq <haqgamer66@gmail.com>
2019-12-15 14:58:10 -0500

    changed adfocus link to adfly

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,7 +133,7 @@ const IndexPage = () => (
           ))}
       </IndexInfoTitle>
       <div>
-        <IndexInfoButton href="http://adfoc.us/4914561">
+        <IndexInfoButton href="http://ad.envyclient.com/1">
           <IndexInfoTextRegular textCase="lower">Download&nbsp;</IndexInfoTextRegular>
           <IndexInfoTextBold textCase="lower">{label}</IndexInfoTextBold>
         </IndexInfoButton>
